8037609aad1fbd26973b701e0621683035567d40
Matt Berther <matt@berther.io>
2019-04-02 22:09:00 -0600

    upgrade jekyll and google api gems

--- Gemfile
+++ Gemfile
@@ -2,12 +2,13 @@ source 'https://rubygems.org'
 
 ruby '2.6.2'
 
-gem 'jekyll', '~> 3.0', '>= 3.0.1'
-gem 'jekyll-sitemap'
-gem 'octopress', '~> 3.0.0.rc.12'
-gem 's3_website'
-gem 'redcarpet'
-gem 'google-api-client'
-gem 'dotenv'
+gem 'jekyll', '~> 3.8', '>= 3.8.5'
+gem 'jekyll-sitemap', '~> 1.3', '>= 1.3.1'
+gem 'octopress', '~> 3.0', '>= 3.0.11'
+gem 's3_website', '~> 3.4'
+gem 'redcarpet', '~> 3.4'
+gem 'googleauth', '~> 0.8.1'
+gem 'google-api-client', '~> 0.28.6'
+gem 'dotenv', '~> 1.0', '>= 1.0.2'
 
-gem 'thor', group: :development
+gem 'thor', '~> 0.20.3', group: :development
--- _plugins/most_popular_filter.rb
+++ _plugins/most_popular_filter.rb
@@ -1,17 +1,25 @@
 require 'dotenv'
 Dotenv.load
 
+require 'googleauth'
+require 'google/apis/analytics_v3'
+
 module Jekyll
   module MostPopularFilter
     def most_popular(input, limit = 10)
-      api ||= GrayIron::GoogleAPI.new('berther.io website', '1.0.0')
-      options = { 'start-date' => '2005-01-01', 'max-results' => limit, 'filters' => 'ga:pagePath=~^\/\d{4}\/\d{2}\/\d{2}[^/]*'}
+      scopes = ['https://www.googleapis.com/auth/analytics.readonly']
 
-      api.most_popular(options).map do |a|
-        a << '/' unless a.end_with?('/')
-        input.find { |post| post.url == a }
+      stats = Google::Apis::AnalyticsV3::AnalyticsService.new
+      stats.authorization = Google::Auth.get_application_default(scopes)
+
+      results = stats.get_ga_data("ga:#{ENV['PROFILE_ID']}", "2005-01-01", DateTime.now.strftime('%Y-%m-%d'),
+                                  "ga:pageviews", dimensions: "ga:pagePath", sort: "-ga:pageviews",
+                                  filters: 'ga:pagePath=~^\/\d{4}\/\d{2}\/\d{2}\/.*\/', max_results: limit)
+
+      results.rows.map do |a|
+        a[0] << '/' unless a[0].end_with?('/')
+        input.find { |post| post.url == a[0] }
       end
-
     end
   end
 end
